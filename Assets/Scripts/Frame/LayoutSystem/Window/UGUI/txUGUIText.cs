a201fb67f71b0427397ca63fd52a703ae75bd03b
zhourui <785130190@qq.com>
2018-08-06 23:52:37 +0800

    添加文件

---
+++ Assets/Scripts/Frame/LayoutSystem/Window/UGUI/txUGUIText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class txUGUIText : txUIObject
+{
+	protected Text mText;
+	public txUGUIText()
+	{
+		mType = UI_TYPE.UT_UGUI_TEXT;
+	}
+	public override void init(GameLayout layout, GameObject go, txUIObject parent)
+	{
+		base.init(layout, go, parent);
+		mText = mObject.GetComponent<Text>();
+		if (mText == null)
+		{
+			mText = mObject.AddComponent<Text>();
+		}
+	}
+	public override int getDepth(){return mText.depth; }
+	public void setText(string text) { mText.text = text; }
+	public string getText() { return mText.text; }
+}

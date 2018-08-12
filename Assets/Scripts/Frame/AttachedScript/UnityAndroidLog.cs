ffc9e9be5fc8301a045a70213126df2ad1b17e83
zhourui <785130190@qq.com>
2018-08-12 23:42:41 +0800

    添加读取安卓Assets文件的接口

---
+++ Assets/Scripts/Frame/AttachedScript/UnityAndroidLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class UnityAndroidLog : MonoBehaviour
+{
+	public void log(string info)
+	{
+		UnityUtility.logInfo("android : " + info, LOG_LEVEL.LL_FORCE);
+	}
+	public void logError(string info)
+	{
+		UnityUtility.logError("android : " + info);
+	}
+}
---
+++ Assets/Scripts/Frame/ResourceManager/AndroidAssetLoader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AndroidAssetLoader : FrameComponent
+{
+	protected static AndroidJavaClass mUnityPlayer;
+	protected static AndroidJavaObject mCurrentActivity;
+	public AndroidAssetLoader(string name)
+		:base(name)
+	{
+		mUnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		mCurrentActivity = mUnityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+	}
+	public override void destroy()
+	{
+		mUnityPlayer.Dispose();
+		mCurrentActivity.Dispose();
+		base.destroy();
+	}
+	// 相对于StreamingAssets的路径
+	public static byte[] loadFile(string path)
+	{
+		byte[] buffer = mCurrentActivity.Call<byte[]>("loadAB", path);
+		return buffer;
+	}
+	public static string loadTextFile(string path)
+	{
+		byte[] buffer = mCurrentActivity.Call<byte[]>("loadAB", path);
+		string str = BinaryUtility.bytesToString(buffer);
+		return str;
+	}
+}
